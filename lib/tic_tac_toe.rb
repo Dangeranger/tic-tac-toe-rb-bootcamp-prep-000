fa8898c009908779e6bfdb37c3c7a42927295123
Joshua James <burke.joshua.james@gmail.com>
2017-09-22 04:02:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
+    puts "CURRENT IS: #{current_player(board)}"
     move(board, index, 'X')
     display_board(board)
   else
