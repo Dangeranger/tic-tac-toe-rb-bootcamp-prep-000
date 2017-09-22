6a769fac6539e0edc71302292b381b8c1b19f320
Joshua James <burke.joshua.james@gmail.com>
2017-09-22 03:48:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,6 @@ end
 
 def full?(board)
   board.any? { |m| [' ', '', nil].include?(m) } ? false : true
-  # board.any? { |m| m == ' ' || m == '' || m.nil? } ? false : true
 end
 
 def draw?(board)
